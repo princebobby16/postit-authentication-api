d92a83231a7be7e5616ac48b3f2e4624ef546486
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2020-12-03 06:19:30 +0000

    added needed files for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: postit-authentication-server
---
+++ vendor/github.com/cristalhq/jwt/errors.go
@@ -0,0 +1,27 @@
+package jwt
+
+import "errors"
+
+// Build and parse errors.
+var (
+	// ErrInvalidKey indicates that key is not valid.
+	ErrInvalidKey = errors.New("jwt: key is not valid")
+
+	// ErrUnsupportedAlg indicates that given algorithm is not supported.
+	ErrUnsupportedAlg = errors.New("jwt: algorithm is not supported")
+
+	// ErrInvalidFormat indicates that token format is not valid.
+	ErrInvalidFormat = errors.New("jwt: token format is not valid")
+
+	// ErrAudienceInvalidFormat indicates that audience format is not valid.
+	ErrAudienceInvalidFormat = errors.New("jwt: audience format is not valid")
+
+	// ErrDateInvalidFormat indicates that date format is not valid.
+	ErrDateInvalidFormat = errors.New("jwt: date is not valid")
+
+	// ErrAlgorithmMismatch indicates that token is signed by another algorithm.
+	ErrAlgorithmMismatch = errors.New("jwt: token is signed by another algorithm")
+
+	// ErrInvalidSignature indicates that signature is not valid.
+	ErrInvalidSignature = errors.New("jwt: signature is not valid")
+)
